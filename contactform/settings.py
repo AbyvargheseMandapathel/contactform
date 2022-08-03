014555a1bc82b88e2b7a23aeb56def204c1815fd
ABY <ABYVARGHESEMANDAPATHEL@GMAIL.COM>
2022-08-03 18:14:20 +0530

    heroku

--- contactform/settings.py
+++ contactform/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,3 +118,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#activating heroku
+django_heroku.settings(locals())
